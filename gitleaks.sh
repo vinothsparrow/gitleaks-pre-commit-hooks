2dc009a9ac5342a5dc00539d693101c3bf710d4d
Vinoth Kumar <vinothkumarj@tekion.com>
2022-01-11 22:15:35 +0530

    issues fixed

--- gitleaks.sh
+++ gitleaks.sh
@@ -10,9 +10,10 @@ if [ "${ARCH_NAME}" = "x86_64" ]; then
     # else
     #     echo "Running on native Intel"
     # fi 
-    ${SCRIPT_DIR}/gitleaks protect --verbose --redact
+    (cd ${PWD} && ${SCRIPT_DIR}/gitleaks protect --verbose --redact --staged)
 elif [ "${ARCH_NAME}" = "arm64" ]; then
-    ${SCRIPT_DIR}/gitleaks-arm64 protect --verbose --redact
+    (cd ${PWD} && ${SCRIPT_DIR}/gitleaks-arm64 protect --verbose --redact --staged)
 else
     echo "Unknown architecture: ${ARCH_NAME}"
+    exit 1
 fi
